0f18c4d9ad2bbd7ff7ed0f616ea1b58bd50ac970
PHANTOM Gaming <mrsanat07@gmail.com>
2025-10-17 01:46:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3490601220-af343",
+  "appId": "1:883866076993:web:8c32358e6eed63575d83c0",
+  "apiKey": "AIzaSyC9J-ftE9Lmn5hEY1OHpFQODt_YTAXTK10",
+  "authDomain": "studio-3490601220-af343.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "883866076993"
+};
